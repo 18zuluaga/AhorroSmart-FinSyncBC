aa0459d39cac5d9f74b960c392f928e7dbda4813
18zuluaga <18zuluaga1989@gmail.com>
2024-11-27 11:50:29 -0500

    refactor auth services

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,20 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({
-    description:
-    
-      'The document number of the user. Should be a unique identifier.',
-    example: 'usuario@correo.com',
-  })
-  @IsString()
+  @IsEmail()
   email: string;
 
-  @ApiProperty({
-    description: 'The password of the user. Should be a string.',
-    example: 'password123456',
-  })
   @IsString()
   password: string;
 }
